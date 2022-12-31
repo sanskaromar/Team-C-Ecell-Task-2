089af796128910c6baf9c34e81736ec25a023d1b
Tushar00002 <102597162+Tushar00002@users.noreply.github.com>
2022-12-31 18:19:56 +0530

    basic template for react app

---
+++ client/e-cell/src/Layout.js
@@ -0,0 +1,13 @@
+//put all your components and pages here in order decided
+
+import React from 'react'
+
+const Layout = () => {
+  return (
+    <div className=''>
+     Hello
+    </div>
+  )
+}
+
+export default Layout
